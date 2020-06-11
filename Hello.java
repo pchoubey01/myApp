ec405a147c6af4a8bb59403fa5021c898ba22fe0
pchoubey01 <66789941+pchoubey01@users.noreply.github.com>
2020-06-11 14:39:54 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Divisha");
+        int myFirstNumber= (2+3)+(5 *10);
+        int mySecondNumber=12;
+        int myThirdNumber=mySecondNumber*2;
+        int myTotal=myFirstNumber+mySecondNumber+myThirdNumber;
+        System.out.println("Total"+myTotal);
+        int myLastOne=1000 -myTotal;
+        System.out.println("Total"+myLastOne);
+
+    }
+}
